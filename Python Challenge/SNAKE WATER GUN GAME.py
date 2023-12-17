0c36377e00d6d9ba09ba3b270c28304414a95efb
REEMA RAJPUT <131301750+ReemaRajput@users.noreply.github.com>
2023-12-17 23:52:54 +0530

    Update SNAKE WATER GUN GAME.py

--- Python Challenge/SNAKE WATER GUN GAME.py
+++ Python Challenge/SNAKE WATER GUN GAME.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
 
 import random
 
@@ -33,10 +28,3 @@ elif score == -1:
     print("You Lose")
 else:
     print("You Won")
-
-
-# In[ ]:
-
-
-
-
